8f4d8807dcab89b4e0ddb06aeb9b0b3793b5810c
jasper-nyambichu <nyambichujasper@gmail.com>
2026-03-13 05:57:16 +0300

    fix: add transaction receipt and detail routes

--- src/routes/transaction.routes.js
+++ src/routes/transaction.routes.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import { getTransactionHistory } from '../controllers/user.controller.js'
+import { getTransactionById, downloadReceipt } from '../controllers/transaction.controller.js'
 import { protect } from '../middleware/auth.middleware.js'
 import { restrictTo } from '../middleware/role.middleware.js'
 
@@ -9,5 +10,7 @@ const router = express.Router()
 router.use(protect, restrictTo('member'))
 
 router.get('/', getTransactionHistory)
+router.get('/:id', getTransactionById)
+router.get('/:id/receipt', downloadReceipt)
 
 export default router
